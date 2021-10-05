078264243fcdca23509cd5cc778b4bb4835d0fd5
Eowyn Peterson <eowynpeterson@Eowyns-MacBook-Air.local>
2021-10-05 15:51:35 -0500

    initial commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,30 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async () => {
+  const res = await fetch(`https://picsum.photos/v2/list?limit=100`);
+  const images = await res.json();
+  selectRandomImage(images);
+};
+
+
+
+const selectRandomImage = (images) => {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  const randomImage = images[randomIndex];
+  console.log(randomImage);
+  displayImage(randomImage);
+};
+
+const displayImage = (randomImage) => {
+  const author = randomImage.author;
+  const imageAddress = randomImage.download_url;
+  authorSpan.textContent = author;
+  img.src = imageAddress;
+  imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", (e) => {
+  getImage();
+});
